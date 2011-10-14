496c6c62ab4f60316be98925d1f947558cc1d106
Dotan Nahum <jondotan@gmail.com>
2011-10-14 14:56:31 +0200

    specing

--- spec/integration/app_spec.rb
+++ spec/integration/app_spec.rb
@@ -3,20 +3,38 @@ require 'helper'
 
 
 describe Moxy::App do
+  SCRIPT=<<-EOF
+    stub_request(:get, "http://example.org").to_return(:body=>"hello")
+  EOF
+
   before do
     set_app Moxy::App
     WebMock.reset!
   end
 
   it "should set up a mock" do
-    script=<<-EOF
-      stub_request(:get, "http://example.org").to_return(:body=>"hello")
-    EOF
-
-    post '/', {:mock_text=>script}
-
+    post '/', {:mock_text=>SCRIPT}
     WebMock::StubRegistry.instance.request_stubs.size.must_equal 1
     WebMock::StubRegistry.instance.request_stubs.first.response.body.must_equal "hello"
   end
+
+  it "should reset mocks" do
+    post '/', {:mock_text => SCRIPT}
+    get  '/reset'
+    WebMock::StubRegistry.instance.request_stubs.size.must_equal 0
+  end
+
+  it "should list out all existing mocks" do
+    post '/', {:mock_text => SCRIPT}
+    get  '/current'
+    r.body.must_match "GET http://example.org"
+  end
+
+  it "should indicate error" do
+    post '/', {:mock_text => "foobar"}
+    get  '/'
+    r.body.must_match "Error registering this request: undefined local variable or method"
+  end
 end
+
 
